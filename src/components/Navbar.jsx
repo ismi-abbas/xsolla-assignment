5096ddeeda513f2dd29834b2d9d729a4c924574d
abbas <abbaspuzi.dev@gmail.com>
2023-03-14 13:16:25 +0800

    link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar({ search }) {
   return (
     <nav className="px-2 sm:px-4 py-2.5 rounded bg-purple-900 flex">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <a href="#" className="flex items-center">
           <img
             src={team17Image}
             className="h-6 mr-3 sm:h-9"
